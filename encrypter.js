2aa7435300c824eab48c2a0c572e2e19922fd586
edunyx <edu.elias.pe@gmail.com>
2024-08-12 04:57:52 -0500

    Actualizando repositorio fv 2.0

--- encrypter.js
+++ encrypter.js
@@ -1,3 +1,23 @@
+function no_space_white1() {
+    let blanco = document.getElementById("texto").value;
+
+    if (blanco.length === 0) {
+        alert("Texto vacío");
+    } else if (blanco.replace(/\s/g, "").length === 0) {
+        alert("Contiene espacios, texto vacío");
+    }
+}
+
+function no_space_white2() {
+    let blanco = document.getElementById("texto2").value;
+
+    if (blanco.length === 0) {
+        alert("Texto vacío");
+    } else if (blanco.replace(/\s/g, "").length === 0) {
+        alert("Contiene espacios, texto vacío");
+    }
+}
+
 function btn_inicial_oculto() {
     document.getElementById('copiar').style.visibility = 'hidden';
 }
@@ -22,13 +42,21 @@ document.getElementById("texto2").addEventListener("input", function() {
 });
 
 function encriptar() {
-    var texto = document.getElementById("texto").value.toLowerCase();
-    var textoCifrado = texto.replace(/e/igm, "enter");
-    textoCifrado = textoCifrado.replace(/i/igm, "imes");
-    textoCifrado = textoCifrado.replace(/a/igm, "ai");
-    textoCifrado = textoCifrado.replace(/o/igm, "ober");
-    textoCifrado = textoCifrado.replace(/u/igm, "ufat");
+    var texto = document.getElementById("texto").value.trim();
 
+    // Verificar si el texto está vacío después de eliminar los espacios en blanco
+    if (texto === "") {
+        return no_space_white1();
+    }
+
+    // Si hay texto, proceder con el cifrado
+    var textoCifrado = texto.replace(/e/igm, "enter")
+                             .replace(/i/igm, "imes")
+                             .replace(/a/igm, "ai")
+                             .replace(/o/igm, "ober")
+                             .replace(/u/igm, "ufat");
+
+    document.getElementById("texto2").rows = 15; // Establecer un número fijo de filas
     document.getElementById("texto2").value = textoCifrado;
     hiddenelementos();
     document.getElementById('copiar').style.visibility = 'visible';
@@ -36,15 +64,23 @@ function encriptar() {
 }
 
 function desencriptar() {
-    var texto = document.getElementById("texto").value.toLowerCase();
-    var textoCifrado = texto.replace(/enter/igm, "e");
-    textoCifrado = textoCifrado.replace(/imes/igm, "i");
-    textoCifrado = textoCifrado.replace(/ai/igm, "a");
-    textoCifrado = textoCifrado.replace(/ober/igm, "o");
-    textoCifrado = textoCifrado.replace(/ufat/igm, "u");
+    var texto = document.getElementById("texto").value.trim();
 
+    // Verificar si el texto está vacío después de eliminar los espacios en blanco
+    if (texto === "") {
+        return no_space_white2();
+    }
+
+    var textoCifrado = texto.replace(/enter/igm, "e")
+                             .replace(/imes/igm, "i")
+                             .replace(/ai/igm, "a")
+                             .replace(/ober/igm, "o")
+                             .replace(/ufat/igm, "u");
+
+    document.getElementById("texto2").rows = 15; // Establecer un número fijo de filas
     document.getElementById("texto2").value = textoCifrado;
     hiddenelementos();
+    
     document.getElementById('copiar').style.visibility = 'visible';
 }
 
@@ -56,6 +92,9 @@ function hiddenelementos() {
 
 function copiar() {
     var contenido = document.getElementById("texto2");
+    if (contenido.value.trim() === "") {
+        return no_space_white2();
+    }
     contenido.select();
     contenido.setSelectionRange(0, 99999); // Para dispositivos móviles
     navigator.clipboard.writeText(contenido.value)
